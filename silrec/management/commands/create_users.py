6fa56d2b937fde7fc6c5e51f0727b66e66d7f3cc
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2026-06-17 13:15:07 +0800

    updated create_users with round1 and round2 roles

--- silrec/management/commands/create_users.py
+++ silrec/management/commands/create_users.py
@@ -4,45 +4,98 @@ from django.contrib.auth.models import Group
 
 User = get_user_model()
 
-USERS = {
-    'Reviewer': [
-        ('Jodie', 'Miller', 'jodie.miller@dbca.wa.gov.au'),
-        ('Matt', 'Seymour', 'matthew.seymour@dbca.wa.gov.au'),
-        ('Lisa', 'Smith', 'lisa.smith@dbca.wa.gov.au'),
-        ('Mel', 'Dybala', 'melanie.dybala@dbca.wa.gov.au'),
-        ('Martin', 'Van Rooyen', 'martin.vanrooyen@dbca.wa.gov.au'),
-        ('Clarissa', 'Swarts', 'clarissa.swarts@dbca.wa.gov.au'),
-        ('Thu', 'Nguyen', 'thu.nguyen@dbca.wa.gov.au'),
+ROUND1 = {
+    'Silrec Admin': [
+        ('Jawaid', 'Mushtaq', 'jawaid.mushtaq@dbca.wa.gov.au'),
+        ('Derek', 'Winters', 'derek.winters@dbca.wa.gov.au'),
+        ('Xianfeng', 'Su', 'xianfeng.su@dbca.wa.gov.au'),
     ],
     'Operator': [
         ('Shelley', 'Phillips', 'shelley.phillips@dbca.wa.gov.au'),
         ('John', 'Mosaj', 'john.mosaj@dbca.wa.gov.au'),
-    ],
-    'Silrec Admin': [
-        ('Derek', 'Winters', 'derek.winters@dbca.wa.gov.au'),
-        ('Zianfeng', 'Su', 'xianfeng.su@dbca.wa.gov.au'),
-        ('Jawaid', 'Mushtaq', 'jawaid.mushtaq@dbca.wa.gov.au'),
         ('Sonia', 'Gillespie', 'sonia.gillespie@dbca.wa.gov.au'),
+        ('Jodie', 'Miller', 'jodie.miller@dbca.wa.gov.au'),
+        ('Matt', 'Seymour', 'matthew.seymour@dbca.wa.gov.au'),
+        ('Ben', 'Davies', 'benton.davies@dbca.wa.gov.au'),
+        ('Cal', 'Burwood', 'callum.burwood@dbca.wa.gov.au'),
+    ],
+    'Reviewer': [
+        ('Melanie', 'Dybala', 'melanie.dybala@dbca.wa.gov.au'),
+        ('Jodie', 'Watts', 'jodie.watts@dbca.wa.gov.au'),
+        ('Bec', 'Brown', 'rebecca.brown@dbca.wa.gov.au'),
+        ('Magda', 'Bustos Hevia', 'magda.bustoshevia@dbca.wa.gov.au'),
+        ('Martin', 'Van Rooyen', 'martin.vanrooyen@dbca.wa.gov.au'),
+        ('Clarissa', 'Swarts', 'clarissa.swarts@dbca.wa.gov.au'),
+    ],
+    'User': [
+        ('Lisa', 'Smith', 'lisa.smith@dbca.wa.gov.au'),
+        ('Prem', 'Neupane', 'prem.neupane@dbca.wa.gov.au'),
+        ('Thu', 'Nguyen', 'thu.nguyen@dbca.wa.gov.au'),
     ],
 }
 
+ROUND2 = {
+    'Silrec Admin': [
+        ('Jawaid', 'Mushtaq', 'jawaid.mushtaq@dbca.wa.gov.au'),
+    ],
+    'Operator': [
+        ('Derek', 'Winters', 'derek.winters@dbca.wa.gov.au'),
+        ('Clarissa', 'Swarts', 'clarissa.swarts@dbca.wa.gov.au'),
+    ],
+    'Reviewer': [
+        ('Xianfeng', 'Su', 'xianfeng.su@dbca.wa.gov.au'),
+        ('Shelley', 'Phillips', 'shelley.phillips@dbca.wa.gov.au'),
+        ('John', 'Mosaj', 'john.mosaj@dbca.wa.gov.au'),
+        ('Sonia', 'Gillespie', 'sonia.gillespie@dbca.wa.gov.au'),
+        ('Jodie', 'Miller', 'jodie.miller@dbca.wa.gov.au'),
+        ('Lisa', 'Smith', 'lisa.smith@dbca.wa.gov.au'),
+        ('Matt', 'Seymour', 'matthew.seymour@dbca.wa.gov.au'),
+        ('Ben', 'Davies', 'benton.davies@dbca.wa.gov.au'),
+        ('Cal', 'Burwood', 'callum.burwood@dbca.wa.gov.au'),
+        ('Prem', 'Neupane', 'prem.neupane@dbca.wa.gov.au'),
+        ('Thu', 'Nguyen', 'thu.nguyen@dbca.wa.gov.au'),
+    ],
+    'User': [
+        ('Melanie', 'Dybala', 'melanie.dybala@dbca.wa.gov.au'),
+        ('Jodie', 'Watts', 'jodie.watts@dbca.wa.gov.au'),
+        ('Bec', 'Brown', 'rebecca.brown@dbca.wa.gov.au'),
+        ('Magda', 'Bustos Hevia', 'magda.bustoshevia@dbca.wa.gov.au'),
+        ('Martin', 'Van Rooyen', 'martin.vanrooyen@dbca.wa.gov.au'),
+    ],
+}
+
+ROUNDS = {'round1': ROUND1, 'round2': ROUND2}
+
 
 class Command(BaseCommand):
-    help = 'Create or remove users defined in USERS'
+    '''
+    # Round 1 (default)                                                                                                                                                                     python manage.py create_users                                                                                                                                                           python manage.py create_users --round round1                                                                                                                                                                                                                                                                                                                                    # Round 2                                                                                                                                                                               python manage.py create_users --round round2                                                                                                                                                                                                                                                                                                                                    # Remove users for a round                                                                                                                                                              python manage.py create_users --remove --round round2
+
+    '''
+    help = 'Create or remove users by round'
 
     def add_arguments(self, parser):
         parser.add_argument(
             '--remove',
             action='store_true',
-            help='Remove users defined in USERS instead of creating them',
+            help='Remove users instead of creating them',
         )
         parser.add_argument(
             '--list',
             action='store_true',
             help='List all users with groups, staff, active status',
         )
+        parser.add_argument(
+            '--round',
+            type=str,
+            choices=['round1', 'round2'],
+            default='round1',
+            help='Which round of users to create/remove (default: round1)',
+        )
 
     def handle(self, *args, **options):
+        self.users = ROUNDS[options['round']]
+        self.round = options['round']
         if options['list']:
             self._list_users()
         elif options['remove']:
@@ -70,7 +123,7 @@ class Command(BaseCommand):
             )
 
     def _create_users(self):
-        for group_name, people in USERS.items():
+        for group_name, people in self.users.items():
             group, _ = Group.objects.get_or_create(name=group_name)
             for first_name, last_name, email in people:
                 username = email
@@ -91,11 +144,11 @@ class Command(BaseCommand):
                         f'Created user {first_name} {last_name} ({email})'
                     ))
 
-                    user.groups.add(group)
-                    self.stdout.write(f'  → added to group "{group_name}"')
+                user.groups.add(group)
+                self.stdout.write(f'  → added to group "{group_name}"')
 
     def _remove_users(self):
-        for group_name, people in USERS.items():
+        for group_name, people in self.users.items():
             for first_name, last_name, email in people:
                 try:
                     user = User.objects.get(username=email)
